93ba4c72599cc56aaae8e444a5b7311ad469dbb7
Luis Felipe Evilla <luisevilla588@gmail.com>
2022-05-16 22:36:02 -0500

    generando los paquetes

--- primerCheckerG#.py
+++ primerCheckerG#.py
@@ -1,7 +1,16 @@
-from mpi4py import MPI
 import sys
 
-print(sys.argv)
+# Recibe el k número hasta donde llegara a comprobar primos
+k = int(sys.argv[1])
+
+paquetes = []
+
+# número de paquetes de 100 en 100 números que se repartiran entre los workers
+num_paquetes = int(k/100)
+
+for i in range(1, num_paquetes + 1):
+    paquete = 100 * i
+    paquetes.append(paquete)
 
 def primeCheck(num):
     divisores = 0
